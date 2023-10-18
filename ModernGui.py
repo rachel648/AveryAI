3e6247fdbcbcd952684dbfb362e361fd402531b0
Swayy03 <samsonmoturi79@gmail.com>
2023-10-18 17:42:45 +0300

    Code for Sam Investments as of 18th October
    Code has been shifted from my previous laptop to the new one. This is before dealing with the Daraja API. What has been done is...
    General design
    Initial connection to the database
    Everything on the tenant management

--- ModernGui.py
+++ ModernGui.py
@@ -13,13 +13,13 @@ class trial:
                 print(item)
 
         idis = []
-        self.treeview1 = ttk.Treeview(self.root, columns=('sth', 'else', 'entirely'), show='headings')
+        self.treeview1 = ttk.Treeview(self.root, columns=('sth', 'else', 'entirely'), show='headings', )
         self.treeview1.heading('sth', text= 'Sth')
         self.treeview1.heading('else', text='Else')
         self.treeview1.heading('entirely', text='Entirely')
         self.treeview1.pack()
         self.treeview1.insert(parent='', index=0,  values=('hello', 'there', 'stanger'))
-        self.somebutton = ctk.CTkButton(self.root, text='button', command=getid)
+        self.somebutton = ctk.CTkButton(self.root, text='button', command=getid, )
         self.somebutton.pack()
 
 
--- Sam_Investments1.py
+++ Sam_Investments1.py
@@ -13,9 +13,11 @@ sectionwidth = 320
 class MyGui:
     j = None
     option = None
+    k = False
 
     def __init__(self):
         # Default properties
+        self.k = MyGui.k
         self.j = MyGui.j
         self.new_tenant = None
         self.updated_iid = None
@@ -39,6 +41,7 @@ class MyGui:
 
         def change_to_main():
             self.tenants.forget()
+            self.expenses.forget()
             self.user = self.username_text.get()
             self.password = self.password_text.get()
 
@@ -56,6 +59,12 @@ class MyGui:
         def tenant_management(event):
             self.tenants.pack(fill="both", expand=True)
             self.mainframe.forget()
+
+        def expense_tracking_event(event):
+            self.expenses.pack(fill="both", expand=True)
+            self.mainframe.forget()
+            self.tenant_management_frame.forget()
+
 
         def actions():
             self.new_tenant = forms.InputDialog()
@@ -88,8 +97,76 @@ class MyGui:
         def getids(event):
             for item in self.tenant_table.selection():
                 print(item)
+        def change_cursor(my_frame):
+            my_frame.configure(cursor="star")
+        def fill_tenant_table():
+            for item in self.tenant_table.get_children():
+                self.tenant_table.delete(item)
+            self.conn_obj = psycopg2.connect(user=MyDatabase.username, password=MyDatabase.pwd, host=MyDatabase.hostname, database='tenants')
+            self.cur_object = self.conn_obj.cursor()
+            self.cur_object.execute('SELECT tenant_id FROM tenantinfo')
+            tenant_ids = self.cur_object.fetchall()
+            self.cur_object.execute('SELECT first_name FROM tenantinfo')
+            first_names = self.cur_object.fetchall()
+            self.cur_object.execute('SELECT last_name FROM tenantinfo')
+            last_names = self.cur_object.fetchall()
+            self.cur_object.execute('SELECT house_no FROM tenantinfo')
+            house_nos = self.cur_object.fetchall()
+            counter = 0
+            fnames = []
+            lnames = []
+            hnos = []
 
+            for hno in house_nos:
+                hnos.append(hno)
 
+            for lname in last_names:
+                lnames.append(lname)
+
+            for fname in first_names:
+                fnames.append(fname)
+
+            for tid in tenant_ids:
+                tenant_id = tid
+                first_name = fnames[counter]
+                last_name = lnames[counter]
+                house_no = hnos[counter]
+                data = (tenant_id, first_name, last_name, house_no)
+                self.tenant_table.insert(parent='', index=counter, values=data , iid=tenant_id)
+                counter = counter + 1
+
+        def search_for_tenant(event):
+            name = self.search_tenant.get()
+            result = None
+            conn_obj = psycopg2.connect(user=MyDatabase.username, password=MyDatabase.pwd, host=MyDatabase.hostname, database='tenants')
+            cur_obj = conn_obj.cursor()
+            cur_obj.execute(f"SELECT * FROM tenantinfo WHERE first_name = '{name}'")
+            result = cur_obj.fetchall()
+            if not result:
+                cur_obj.execute(f"SELECT * FROM tenantinfo WHERE last_name = '{name}'")
+                if not result:
+                    print("Not found")
+                else:
+                    pass
+            else:
+
+                for name1 in first_names:
+                    name2 = name1[0]
+                    if name2 == name:
+                        cur_obj.execute(f"SELECT tenant_id FROM tenantinfo WHERE first_name = '{name}'")
+                        id_late = cur_obj.fetchone()
+                for ten_id in tenant_ids:
+                    print(ten_id[0])
+                    self.tenant_table.focus(ten_id[0])
+                    item = self.tenant_table.focus()
+                    id_late1 = str(id_late[0])
+                    print(id_late1 + ":::::" + item)
+
+                    if item == id_late1:
+                        print("I am mr meeseeks look at me")
+                    else:
+                        self.tenant_table.delete(ten_id)
+                self.k = True
 
         # Widgets#########################
         # framessss
@@ -98,6 +175,7 @@ class MyGui:
 
         self.mainframe = ctk.CTkFrame(master=self.root)
         self.tenants = ctk.CTkFrame(master=self.root)
+        self.expenses = ctk.CTkFrame(master=self.root)
         # Actual widgets
         self.main_label = ctk.CTkLabel(self.loginframe, text="SAM INVESTMENTS RENTAL MANAGEMENT",
                                        font=('Lithos Pro Regular', 30), text_color='green')
@@ -133,15 +211,21 @@ class MyGui:
 
         self.expense_tracking_frame = ctk.CTkFrame(self.mainframe, fg_color="#0e1b52", height=300, width=sectionwidth)
         self.expense_tracking_frame.pack_propagate(False)
+        self.expense_tracking_frame.bind("<Enter>", command=change_cursor(self.expense_tracking_frame))
+        self.expense_tracking_frame.bind("<Button-1>", expense_tracking_event)
+        self.expense_tracking_frame.bind("<Enter>", command=lambda e: self.expense_tracking_frame.configure(fg_color="green", height=310, width=(sectionwidth+10)))
+        self.expense_tracking_frame.bind("<Leave>", command=lambda e: self.expense_tracking_frame.configure(fg_color="#0e1b52", height=300, width=sectionwidth))
         self.expense_tracking_frame.grid(row=0, column=1, padx=10, pady=10)
 
-        self.expense_tracking_label = ctk.CTkLabel(self.expense_tracking_frame, text="Expense tracking",
-                                                   font=('Arial', 16))
+        self.expense_tracking_label = ctk.CTkLabel(self.expense_tracking_frame, text="Expense tracking", font=('Arial', 16))
         self.expense_tracking_label.pack(padx=10, pady=10)
 
         self.tenant_management_frame = ctk.CTkFrame(self.mainframe, fg_color="#0e1b52", height=300, width=sectionwidth)
         self.tenant_management_frame.pack_propagate(False)
+        self.tenant_management_frame.bind("<Enter>", command=change_cursor(self.tenant_management_frame))
         self.tenant_management_frame.bind("<Button-1>", tenant_management)
+        self.tenant_management_frame.bind("<Enter>", command= lambda e: self.tenant_management_frame.configure(fg_color="green", height=310, width=(sectionwidth+10)))
+        self.tenant_management_frame.bind("<Leave>", command= lambda e: self.tenant_management_frame.configure(fg_color="#0e1b52", height=300, width=sectionwidth))
         self.tenant_management_frame.grid(row=1, column=1, padx=10, pady=10)
 
         self.tenant_management_label = ctk.CTkLabel(self.tenant_management_frame, text="Tenant Management",
@@ -150,6 +234,9 @@ class MyGui:
 
         self.monitoring_frame = ctk.CTkFrame(self.mainframe, fg_color="#0e1b52", height=300, width=sectionwidth)
         self.monitoring_frame.pack_propagate(False)
+        self.monitoring_frame.bind("<Enter>", command=change_cursor(self.monitoring_frame))
+        self.monitoring_frame.bind("<Enter>", command=lambda e: self.monitoring_frame.configure(fg_color="green", height=310, width=(sectionwidth+10)))
+        self.monitoring_frame.bind("<Leave>", command=lambda e: self.monitoring_frame.configure(fg_color="#0e1b52", height=300, width=sectionwidth))
         self.monitoring_frame.grid(row=0, column=2, padx=10, pady=10)
 
         self.monitoring_label = ctk.CTkLabel(self.monitoring_frame, text="Electricity and water monitoring",
@@ -158,6 +245,9 @@ class MyGui:
 
         self.CCTV_frame = ctk.CTkFrame(self.mainframe, fg_color="#0e1b52", height=300, width=sectionwidth)
         self.CCTV_frame.pack_propagate(False)
+        self.CCTV_frame.bind("<Enter", change_cursor(self.CCTV_frame))
+        self.CCTV_frame.bind("<Enter>", command=lambda e: self.CCTV_frame.configure(fg_color="green", height=310, width=(sectionwidth+10)))
+        self.CCTV_frame.bind("<Leave>", command=lambda e: self.CCTV_frame.configure(fg_color="#0e1b52",  height=300, width=sectionwidth))
         self.CCTV_frame.grid(row=1, column=2, padx=10, pady=10)
 
         self.CCTV_label = ctk.CTkLabel(self.CCTV_frame, text="CCTV interface", font=('Arial', 16))
@@ -165,6 +255,9 @@ class MyGui:
 
         self.SMS_automation_frame = ctk.CTkFrame(self.mainframe, fg_color="#0e1b52", height=300, width=sectionwidth)
         self.SMS_automation_frame.pack_propagate(False)
+        self.SMS_automation_frame.bind("<Enter>", command=change_cursor(self.SMS_automation_frame))
+        self.SMS_automation_frame.bind("<Enter>", command=lambda e: self.SMS_automation_frame.configure(fg_color="green", height=310, width=(sectionwidth+10)))
+        self.SMS_automation_frame.bind("<Leave>", command=lambda e: self.SMS_automation_frame.configure(fg_color="#0e1b52", height=300, width=sectionwidth))
         self.SMS_automation_frame.grid(row=0, column=3, padx=10, pady=10)
 
         self.SMS_automation_label = ctk.CTkLabel(self.SMS_automation_frame, text="SMS Automation", font=('Arial', 16))
@@ -172,16 +265,25 @@ class MyGui:
 
         self.token_monitoring_frame = ctk.CTkFrame(self.mainframe, fg_color="#0e1b52", height=300, width=sectionwidth)
         self.token_monitoring_frame.pack_propagate(False)
+        self.token_monitoring_frame.bind("<Enter>", command=change_cursor(self.token_monitoring_frame))
+        self.token_monitoring_frame.bind("<Enter>", command=lambda e: self.token_monitoring_frame.configure(fg_color="green", height=310, width=(sectionwidth+10)))
+        self.token_monitoring_frame.bind("<Leave>", command=lambda e: self.token_monitoring_frame.configure(fg_color="#0e1b52", height=300, width=sectionwidth))
         self.token_monitoring_frame.grid(row=1, column=3, padx=10, pady=5)
 
-        self.token_monitoring_label = ctk.CTkLabel(self.token_monitoring_frame, text="Token Monitoring",
-                                                   font=('Arial', 16))
+        self.token_monitoring_label = ctk.CTkLabel(self.token_monitoring_frame, text="Token Monitoring", font=('Arial', 16))
         self.token_monitoring_label.pack(padx=10, pady=10)
 
-        self.tenants_label = ctk.CTkLabel(self.tenants, text="TENANT MANAGEMENT",font=('Times', 30), text_color="yellow")
+        self.loginreturnbutton = ctk.CTkButton(master=self.sideframe, text="Return", font=('Arial', 16), command=return_to_login)
+        self.loginreturnbutton.pack(side=tk.TOP, padx=10, pady=5)
+
+        # Tenants Frame!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        self.tenants_label = ctk.CTkLabel(self.tenants, text="TENANT MANAGEMENT",font=('Times', 30), text_color="green")
         self.tenants_label.pack(pady=5)
-        self.entry1 = ctk.CTkEntry(self.tenants, placeholder_text="Search", height=10, width=250, justify=tk.CENTER)
-        self.entry1.pack(padx=10, pady=25)
+        self.search_tenant = ctk.CTkEntry(self.tenants, placeholder_text="Search", height=10, width=250, justify=tk.CENTER)
+        self.search_tenant.pack(padx=10, pady=(20, 10))
+        self.search_tenant.bind("<Return>", search_for_tenant)
+        self.search_tenantbutton = ctk.CTkButton(self.tenants, text="Clear", width=100, command=fill_tenant_table)
+        self.search_tenantbutton.pack(padx=10, pady=10)
 
         self.tenant_table = ttk.Treeview(self.tenants, columns=('tenant_id', 'First_name', 'Last_name', 'House_no'), show='headings', height=20)
         self.tenant_table.heading('tenant_id', text='Tenant_Id')
@@ -190,27 +292,70 @@ class MyGui:
         self.tenant_table.heading('House_no', text='House_No')
         self.tenant_table.bind("<Delete>", delete_tenant)
         self.tenant_table.bind("<<TreeviewSelect>>", getids)
-        self.tenant_table.pack()
+        self.tenant_table.pack(pady=10)
 
         self.datalist = ctk.CTkLabel(self.tenants, text="", text_color="blue")
         self.datalist.pack()
 
+        self.mainreturnbutton = ctk.CTkButton(master=self.tenants, text="Return", font=('Arial', 14), command=change_to_main)
+        self.mainreturnbutton.pack(pady=5, side=tk.BOTTOM)
 
-
-        self.loginreturnbutton = ctk.CTkButton(master=self.sideframe, text="Return", font=('Arial', 16), command=return_to_login)
-        self.loginreturnbutton.pack(side=tk.TOP, padx=10, pady=10)
-
-
-        self.mainreturnbutton = ctk.CTkButton(master=self.tenants, text="Return", font=('Arial', 20), command=change_to_main)
-        self.mainreturnbutton.pack(pady=10, side=tk.BOTTOM)
-
-        self.create_button = ctk.CTkButton(master=self.tenants, text="Actions", font=('Roboto', 14), command=actions)
+        self.create_button = ctk.CTkButton(master=self.tenants, text="Actions", font=('Roboto', 14), command=actions, height=30)
         self.create_button.bind("<Button-1>", actions)
-        self.create_button.pack(pady=10, side=tk.BOTTOM)
+        self.create_button.pack(pady=5, side=tk.BOTTOM)
+
+        # Expense Tracking frame
+        self.mpesa_balance = 300
+        self.bank_balance = 26000
+
+
+        self.bank_account_frame = ctk.CTkFrame(self.expenses, height=200, width=500, fg_color="#0B0B45", corner_radius=10)
+        self.bank_account_frame.grid_propagate(False)
+        self.bank_account_frame.pack_propagate(False)
+        self.bank_account_frame.grid(row=1, column=1, columnspan=40, padx=20, pady=40)
+
+        self.bank_account_label = ctk.CTkLabel(self.bank_account_frame, height=10, text="Bank Account Balance", font=('Roboto', 20), text_color="#00FF0F")
+        self.bank_balance_label = ctk.CTkLabel(self.bank_account_frame, text=f"Kshs.{self.bank_balance}", font=('Roboto', 15))
+        self.bank_balance_label.pack(padx=10, pady=10, side=tk.BOTTOM)
+        self.bank_account_label.grid(row=1, column=1, columnspan=30, padx=10, pady=10)
+
+        self.mpesa_account_frame = ctk.CTkFrame(self.expenses, height=200, width=500, fg_color="#0B0B45", corner_radius=10)
+        self.mpesa_account_frame.grid_propagate(False)
+        self.mpesa_account_frame.pack_propagate(False)
+        self.mpesa_account_frame.grid(row=1, column=41, columnspan=40, padx=20, pady=40)
+
+        self.mpesa_account_label = ctk.CTkLabel(self.mpesa_account_frame, height=10, text="Mpesa Account Balance", font=('Roboto', 20), text_color="#00FF0F")
+        self.mpesa_balance_label = ctk.CTkLabel(self.mpesa_account_frame, text=f"Kshs.{self.mpesa_balance}", font=("Roboto", 15))
+        self.mpesa_balance_label.pack(padx=10, pady=10, side=tk.BOTTOM)
+        # self.mpesa_balance_label.grid(row=100, column=20, columnspan=20, padx=10, pady=30)
+        # self.mpesa_account_label.pack(padx=10, pady=10)
+        self.mpesa_account_label.grid(row=1, column=1, columnspan=30, padx=10, pady=10)
+
+        self.data_analysis_label = ctk.CTkLabel(self.expenses, text="Data Analysis", font=('Arial', 25), text_color="white")
+        self.data_analysis_label.grid(row=2, column=0, padx=10, pady=10, columnspan=15)
+
+        self.target_track = ctk.CTkFrame(self.expenses, height= 300, width= 350)
+        self.target_track.grid(row=3, rowspan = 10 ,column = 1, columnspan = 10, padx=20, pady = 10)
+
+        self.data_track = ctk.CTkFrame(self.expenses, height=250, width = 250, corner_radius=20 )
+        self.data_track.grid(row=4, rowspan=10, column=1, columnspan=10, padx=20, pady=10 )
+
+        self.expensereturn_button = ctk.CTkButton(self.expenses, text="Return", font=('Roboto', 14), command=change_to_main, height=30 )
+        self.expensereturn_button.grid(row=100, column=2, columnspan=4, padx=10, pady=10)
+
+
+
+
+
+
+
 
 
         # Tenants table put it here ndo isishinde imejicall having a redundant treeview
-        self.conn_obj = psycopg2.connect(user=MyDatabase.username, password=MyDatabase.pwd, host=MyDatabase.hostname, database='tenants')
+        for item in self.tenant_table.get_children():
+            self.tenant_table.delete(item)
+        self.conn_obj = psycopg2.connect(user=MyDatabase.username, password=MyDatabase.pwd, host=MyDatabase.hostname,
+                                         database='tenants')
         self.cur_object = self.conn_obj.cursor()
         self.cur_object.execute('SELECT tenant_id FROM tenantinfo')
         tenant_ids = self.cur_object.fetchall()
@@ -240,9 +385,8 @@ class MyGui:
             last_name = lnames[counter]
             house_no = hnos[counter]
             data = (tenant_id, first_name, last_name, house_no)
-            self.tenant_table.insert(parent='', index=counter, values=data , iid=tenant_id)
+            self.tenant_table.insert(parent='', index=counter, values=data, iid=tenant_id)
             counter = counter + 1
-
         if self.j:
             update_tenant1()
 
